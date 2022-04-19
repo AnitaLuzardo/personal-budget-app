b032fd6b40a4033d83319dbb907c5a21ef6a4f0e
Ana Luzardo <anitaebratt@gmail.com>
2022-04-19 15:12:52 -0300

    Maqueta vista login

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const bodyParser = require ('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-const publicPath = path.resolve (__dirname, './public');
+const publicPath = path.resolve (__dirname, '../public');
 app.use(express.static(publicPath));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.use(session({
     secret: 'Secret',
@@ -19,4 +23,4 @@ app.set('views', './src/views');
 
 app.use('/', authRoutes);
 
-app.listen(3000, () => console.log('Servidor corriendo, http://localhost:3000'));
+app.listen(3001, () => console.log('Servidor corriendo, http://localhost:3000'));
